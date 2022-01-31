114a93a10262b534d27db16d70e652d894143a0c
codbop <ismaill.bass95@gmail.com>
2022-01-31 03:20:29 +0300

    İkinci madde de tamamlandı. 1. bloğa oyun yerleştirilebiliyor. Ardından ikinci bloğa da random olarak oyun yerleştirilebiliyor. Ayrıca screenshotlar slider ile kaydırılabiliyor.

--- core/static/js/random_match.js
+++ core/static/js/random_match.js
@@ -1,6 +1,13 @@
+
+
 /* Seçim yapıldığında post requesti ajax ile atılacak */
 $(document).ready(function() {
+
+    /* Döküman ilk açıldığında önce 1.bloğa oyun yerleştirilmeli sonra ikinci bloğa
+    oyun yerleştirilmeli bu yüzden randomize butonunu sayfa ilk açıldığında disable ediyoruz. */
+    document.getElementById('randomize').disabled=true;
     $('#select-app').change(function(){
+
         /* Uygulama idsine erişebilmek için datalist üzerinde option taglerine bu şekilde
         bir filtreleme yapıldı. Çünkü normal value değerleri html üzerinde gözükmekte.
         Bize lazım olan gözükmeyen id değerleri. */
@@ -12,29 +19,87 @@ $(document).ready(function() {
             type: "POST",
             dataType: "json",
             url:window.location.href,
-            data: {'app_id':app_id},
+            data: {'app_id':app_id, 'action':'select_app'},
             success: function(data)
             {
+
                 /* imajları htmlye basmak için {% static %} yerine direk dosya dizin yapısı kullanıldı (../static/images/icons/
                 gibi) çünkü static yapısı özel django karakterleri içermekte (% veya {} gibi) ve innerhtml bu karakterler
                 server tarafında basıldığı için htmlye django karakterleri olarak geçirilemiyor sadece string olarak
                 geçiyorlar */
-                console.log(data.icon);
-                var html_1 =  `<div class="carousel-item active"><img src="../static/images/icons/` + data.icon + `" 
-                        class="d-block w-100" alt="..." style="height:450px;"></div>`;
+                
+                var html_1 = `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner"><div class="carousel-item active"><img src="../static/images/icons/` 
+                + data.icon + `" class="d-block w-100" alt="..." style="height:415px;"></div>`;
                 var html_2 = '';
                 for(const ss of data.ss_list) {
                     html_2 +=
                     `<div class="carousel-item">
-                    <img src="../static/images/ss/` + ss + `" class="d-block w-100" alt="..." style="height:450px;">
+                    <img src="../static/images/ss/` + ss + `" class="d-block w-100" alt="..." style="height:415px;">
                     </div>`;
                 }
-                var new_html = html_1 + html_2;
-                console.log(new_html);
+                html_3 = `</div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                    </button>
+                </div>`
+                var new_html = html_1 + html_2 + html_3;
                 document.getElementById('carousel-1').innerHTML = new_html;
+
+                /* Oyun seçimi yapıldığında ve birinci bloğa oyun yerleştirildiğinde artık 2.bloğa
+                oyun yerleştirilebilir. Bu yüzden randomize butonunu oyun seçtiken sonra aktif ediyoruz. */
+                document.getElementById('randomize').disabled=false;
             },
             
         })
     });
+    $('#randomize').click(function(){
+
+        /* uygulama idsi random geleceği için burada id çekmemize gerek yok.
+        Diğer işlemler bir önceki aksiyonla hemen hemen aynı olacak*/
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url:window.location.href,
+            data: {'action':'randomize'},
+            success: function(data)
+            {
+
+                /* imajları htmlye basmak için {% static %} yerine direk dosya dizin yapısı kullanıldı (../static/images/icons/
+                gibi) çünkü static yapısı özel django karakterleri içermekte (% veya {} gibi) ve innerhtml bu karakterler
+                server tarafında basıldığı için htmlye django karakterleri olarak geçirilemiyor sadece string olarak
+                geçiyorlar */
+                var html_1 = `<div id="carouselExampleControls2" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner"><div class="carousel-item active"><img src="../static/images/icons/` 
+                + data.icon + `" class="d-block w-100" alt="..." style="height:415px;"></div>`;
+                var html_2 = '';
+                for(const ss of data.ss_list) {
+                    html_2 +=
+                    `<div class="carousel-item">
+                    <img src="../static/images/ss/` + ss + `" class="d-block w-100" alt="..." style="height:415px;">
+                    </div>`;
+                }
+                html_3 = `</div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                    </button>
+                </div>`
+                var new_html = html_1 + html_2 + html_3;
+                document.getElementById('carousel-2').innerHTML = new_html;
+            },
+            
+        })
+    })
 });
 
--- core/views.py
+++ core/views.py
@@ -4,9 +4,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 def login(request):
+
     # Sayfa request edildiğinde login penceresi gelsin 
     if request.method == 'GET':
         return render(request, 'login.html')
+
     # Login html üzerinde POST işlemi yapılırsa htmlde form üzerinden gönderilen
     # parametreler üzerinden işlem yapılsın ve kullanıcı authenticate edilsin.
     # kullanıcı authenticate olursa random_match sayfasına erişebilsin.
@@ -16,6 +18,7 @@ def login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         try:
+
             # Kullanıcı tablosunda bu parametrelerle kayıtlı olan bir kullanıcı olup olmadığı
             # kontrol edilir. Eğer var ise sisteme giriş yapılır ve session'da bu parametreler
             # tutulur. Yok ise de kullanıcı yeniden login sayfasına yönlendirilir ve tekrar 
@@ -32,41 +35,74 @@ def login(request):
 def random_match(request):
     if request.method == 'GET':
         if request.session.get('is_authenticated'):
-            """ Eğer sayfa üzerinde get metodu çağrılmışsa blokta henüz bir oyun
-            gözükmemesi gerektiğinden yani önce kullanıcı bir oyun seçmesi ve sonra
-            blokta bir oyun gözükmesi gerektiğinden isCarouselEmpty boolean değişkeni
-            tanımlanmış olup bu değişkenin boolean değerine göre bloğa oyun yerleştirile-
-            cektir. Bu da post metodunda bu değişkenin False olarak gönderilmesi ile sağlana
-            caktır. """
+            """ Sayfa ilk yüklendiğinde htmldeki dropdowna uygulamalar yüklenmelidir.
+            Bu yüzden get metodu çağrıldığında veritabanından uygulamaları çekiyoruz
+            ve context içerisinde htmlye gönderiyoruz."""
             context = {
                 'apps':SampleApps.objects.all(),
-                'isCarouselEmpty':False
             }
             return render(request, 'random_match.html', context)
         else:
             return redirect('/')
+
     # Eğer sayfa üzerinde post requesti atılmış ise burası çalışır
     else:
+
         # Eğer sistemde mevcut olan bir uygulama adı girilmişse buradan uygulama 
         # sorgulanıp döndürülür. Mevcut olmayan bir uygulma girilmiş ise de herhangi
-        # bir işlem yapılmaz.
-        if request.POST.get('app_id'):
+        # bir işlem yapılmaz. Ayrıca bir uygulama seçilmiş ise bu aksiyon uygulama seçme aksiyonudur.
+        # Yani 1. bloğa yerleştirme aksiyonudur. Bu yüzden aksiyon parametresi de kontrol edilmektedir.
+        if request.POST.get('app_id') and request.POST.get('action') == 'select_app':
             app_id = request.POST.get('app_id')
+
             # Seçilen uygulamayı post edilen id ile veritabanı üzerinden
             # sorgulatıyoruz ve bu uygulmaya ait gerekli screenshotların hepsini
             # veritabanından sorgulatıp response olarak döndürüyoruz.
             app = SampleApps.objects.get(id=app_id)
             ss = app.Screenshots.all()
+
             # Uygulamaya ait screenshotları veritabanında sorgulatıp liste olarak elde ettik
             # liste içerisinde tuplelar var ise ve biz böyle bir yapı ile tupleı toplar isek
             # flatten edilmiş bir tuple elde ederiz ve bu tupleı listeye dönüştürerek istediğimiz
             # screenshotları tek bir liste içerisinde elde edebiliriz.
             ss_list = list(sum(ss.values_list('file_name'),()))
-            print(ss_list)
+
             # Responseu ajaxa döndürüyoruz ve ayrıca screenshotların yanında oyunun ana görselini de
             # response olarak döndürüyoruz.
             return JsonResponse({'ss_list':ss_list,
                                 'icon':app.icon})
+
+        # Aksiyon randomize ise ikinci bloğa yerleştirme işlemidir. Bu yüzden bu blok çalışır.
+        elif request.POST.get('action') == 'randomize':
+
+            # Burada da birinci bloğa yerleştirmek için yapılan işlemlerin benzerleri yapılacak.
+            # Tek fark id'nin random olarak üretilmesi ve bir önceki id'nin mevcut id'ye eşit olmaması
+
+            # Veritabanından uygulamaları rastgele sıralarız ve ilk kaydı döndürürüz. Böylece 
+            # rastgele bir oyun elde etmiş oluruz. 
+            # NOT!!: Bu metod çok performanslı değildir ancak veritabanında şuan çok az sayıda
+            # kayıt bulunduğundan kullanılmasında sakınca görülmemiştir.
+
+            # Ayrıca random olarak veritabanından çekilen oyun bir önceki oyuna eşit olmayıncaya
+            # kadar yeniden üretilir ki bir önceki oyundan farklı bir oyun elde edilsin. 
+            app = SampleApps.objects.order_by("?").first()
+            while app.id == request.session.get('is_same'):
+                app = SampleApps.objects.order_by("?").first()
+            
+            # while fonksiyonundan çıkıldığında is_same parametresi mevcut uygulama ile güncellenir
+            # böylece bir sonraki uygulama üretilirken bir sonraki uygulama da bu uygulama ile
+            # karşılaştırılır. Ve bu işlem bu şekilde devam eder ve böylece aynı oyunu hiçbir zaman
+            # elde etmemiş oluruz. Güncellemeyi sessiona kaydederek yapıyoruz.
+            request.session['is_same'] = app.id
+
+            # Geri kalan işlemler bir önceki aksiyonda uygulanan işlemler ile aynı olacaktır.
+            ss = app.Screenshots.all()   
+            ss_list = list(sum(ss.values_list('file_name'),())) 
+
+            return JsonResponse({'ss_list':ss_list,
+                    'icon':app.icon})
+
+            
 
 
         
